02d1347b2431fe63960b7c4268ec9d6805a995a6
Cihangir Akturk <cakturk@gmail.com>
2015-12-30 21:51:09 +0200

    add tcp print method
    Signed-off-by: Cihangir Akturk <cakturk@gmail.com>

--- netsniff.h
+++ netsniff.h
@@ -123,6 +123,43 @@ struct udphdr {
 };
 #define udp_hdr(ptr) ((struct udphdr *)(ptr))
 
+/* TCP segment */
+struct tcphdr {
+	uint16_t  source;
+	uint16_t  dest;
+	uint32_t  seq;
+	uint32_t  ack_seq;
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+	uint16_t  res1:4,
+		  doff:4,
+		  fin:1,
+		  syn:1,
+		  rst:1,
+		  psh:1,
+		  ack:1,
+		  urg:1,
+		  ece:1,
+		  cwr:1;
+#elif defined(__BIG_ENDIAN_BITFIELD)
+	uint16_t  doff:4,
+		  res1:4,
+		  cwr:1,
+		  ece:1,
+		  urg:1,
+		  ack:1,
+		  psh:1,
+		  rst:1,
+		  syn:1,
+		  fin:1;
+#else
+#error  "Adjust your <endian.h> defines"
+#endif
+	uint16_t  window;
+	uint16_t  check;
+	uint16_t  urg_ptr;
+};
+#define tcp_hdr(ptr) ((struct tcphdr *)(ptr))
+
 /* returns true if MF bit is set */
 static inline int ip_mf(struct iphdr *iph)
 {
